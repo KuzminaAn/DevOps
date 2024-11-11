9f3f2a7b24f1b822ec5b7268333350308165bb81
KuzminaAn <anyakuzmina@mail.ru>
2024-11-11 14:02:03 +0300

    удалила locals

--- ter-homeworks/03/src/ansible.tf
+++ ter-homeworks/03/src/ansible.tf
@@ -1,35 +1,29 @@
-locals {
-  webservers = [
-    for inst in yandex_compute_instance.web : {
-      name        = inst.name
-      external_ip = inst.network_interface[0].nat_ip_address
-      fqdn        = format("%s.%s.%s", inst.hostname != "" ? inst.hostname : format("%s-%d", inst.name, index(yandex_compute_instance.web, inst)),var.default_zone, var.vm_fqdn)
-    }
-  ]
-
-  databases = [
-    for inst in yandex_compute_instance.database : {
-      name        = inst.name
-      external_ip = inst.network_interface[0].nat_ip_address
-      fqdn        = format("%s.%s.%s", inst.hostname != "" ? inst.hostname : format("%s-%d", inst.name, index(yandex_compute_instance.database, inst)),var.default_zone, var.vm_fqdn)
-
-    }
-  ]
-
-  storages = [{
-    name = yandex_compute_instance.storage.name
-    external_ip = yandex_compute_instance.storage.network_interface[0].nat_ip_address
-    fqdn        = format("%s.%s.%s", yandex_compute_instance.storage.hostname != "" ? yandex_compute_instance.storage.hostname : format("%s-%d", yandex_compute_instance.storage.name),var.default_zone, var.vm_fqdn)
-    }
-  ]
- }
-
 resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/hosts.tftpl", {
-    webservers = local.webservers,
-    databases = local.databases
-    storages = local.storages
-  })
+    webservers = [
+      for inst in yandex_compute_instance.web : {
+        name        = inst.name
+        external_ip = inst.network_interface[0].nat_ip_address
+        fqdn        = format("%s.%s.%s", inst.hostname != "" ? inst.hostname : format("%s-%d", inst.name, index(yandex_compute_instance.web, inst)), var.default_zone, var.vm_fqdn)
+      }
+    ]
+
+    databases = [
+      for inst in yandex_compute_instance.database : {
+        name        = inst.name
+        external_ip = inst.network_interface[0].nat_ip_address
+        fqdn        = format("%s.%s.%s", inst.hostname != "" ? inst.hostname : format("%s-%d", inst.name, index(yandex_compute_instance.database, inst)), var.default_zone, var.vm_fqdn)
+      }
+    ]
+
+    storages = [{
+      name = yandex_compute_instance.storage.name
+      external_ip = yandex_compute_instance.storage.network_interface[0].nat_ip_address
+      fqdn        = format("%s.%s.%s", yandex_compute_instance.storage.hostname != "" ? yandex_compute_instance.storage.hostname : format("%s-%d", yandex_compute_instance.storage.name), var.default_zone, var.vm_fqdn)
+    }
+    ]
+  }
+  )
 
   filename = "${path.module}/ansible_inventory.ini"
 }
